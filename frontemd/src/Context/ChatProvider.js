cda6b54c8aeed9d57eae05ebd121b8a0d36bfe6e
VRAJ  TALATI <vrajtalatii@gmail.com>
2023-11-18 14:12:08 +0530

    Update ChatProvider.js

--- frontemd/src/Context/ChatProvider.js
+++ frontemd/src/Context/ChatProvider.js
@@ -16,7 +16,7 @@ const ChatProvider = ({ children }) => {
     setUser(userInfo);
 
     if (!userInfo) history.push("/");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+   
   }, [history]);
 
   return (
